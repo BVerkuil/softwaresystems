ea213a2ce8d6f411338713b982179785161a0ec3
Justin Altena <psvjustin@gmail.com>
2015-12-14 15:44:42 +0100

    JML bij MapUtil

--- module 2/src/ss/week5/MapUtil.java
+++ module 2/src/ss/week5/MapUtil.java
@@ -7,6 +7,9 @@ import java.util.Map;
 import java.util.Set;
 
 public class MapUtil {
+		//@ ensures result == true;
+		//@ requires map.keySet().size() == map.values().size();
+		//@ requires map != null;
     public static <K, V> boolean isOneOnOne(Map<K, V> map) {
         boolean result = true;
         Object[] keyList = map.keySet().toArray();
@@ -26,7 +29,8 @@ public class MapUtil {
         }
         return result;
     }
-
+    //@requires map != null && range != null
+    //@ ensures result == true;
     public static <K, V>  boolean isSurjectiveOnRange(Map<K, V> map, Set<V> range) {
 		boolean result = true;
     	for (Object e : range){
@@ -36,7 +40,10 @@ public class MapUtil {
 		}
 		return result;
 	}
-   
+    
+    //@requires map != null
+    //@ensures \forall K key && map.containsKey(key)&& \result.containsValue(Set.contains(key));
+    //@ensures \forall V value; map.containsValue(value); \result.containsKey(value);
     public static <K, V> Map<V, Set<K>> inverse(Map<K, V> map) {
 		Map<V, Set<K>> result = new HashMap<V, Set<K>>();
 		for (K k : map.keySet()){
@@ -47,6 +54,8 @@ public class MapUtil {
 		return result;
 
 	}
+    
+    //@ ensures \result instanceof Map<V, K>;
 	public static <K, V> Map<V, K> inverseBijection(Map<K, V> map) {
 		Map<V, K> result = new HashMap<V, K>();
 			for(K k : map.keySet()) {
@@ -54,6 +63,9 @@ public class MapUtil {
 			}
         return result;
 	}
+	
+	//@requires g != null && f != null
+	//@ensures \result == \forall V value; f.containsValue(value); g.containsKey(value);
 	public static <K, V, W> boolean compatible(Map<K, V> f, Map<V, W> g) {
         boolean result = true;
 		for(V v : f.values()) {
@@ -64,6 +76,9 @@ public class MapUtil {
 		
         return result;
 	}
+	
+	//@ requires compatible(f, g);
+	//@ ensures \result != null;
 	public static <K, V, W> Map<K, W> compose(Map<K, V> f, Map<V, W> g) {
 		Map<K, W> result = new HashMap<K, W>();
 		if(!compatible(f, g)) {
